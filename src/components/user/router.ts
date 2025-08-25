ca429f78ad856c9c37bd57d3b307bb409fd618a2
Vasil Sarandev <vasil.sarandev@gmail.com>
2025-08-25 19:01:34 +0300

    formatting

--- src/components/user/router.ts
+++ src/components/user/router.ts
@@ -5,5 +5,6 @@ import { authMiddleware } from '@/middlewares/auth';
 export const userRouter = Router();
 
 userRouter.use(authMiddleware);
+
 userRouter.get('/me', userController.getCurrentUser);
 userRouter.get('/:id', userController.findUserById);
