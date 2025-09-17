dd00dc5746078bf01569ff86aa52d169ae91c8e7
Vasil Sarandev <vasil.sarandev@gmail.com>
2025-09-17 13:31:20 +0300

    move webhook signature verification to the stripe service

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -5,11 +5,28 @@ export type IPaymentIntentMetadata<T> = T & {
   [key: string]: string;
 };
 
+export const STRIPE_SIGNATURE_HEADER = 'stripe-signature';
+export const STRIPE_EVENT_PAYMENT_SUCCEEDED = 'payment_intent.succeeded';
+export const STRIPE_EVENT_PAYMENT_CANCELED = 'payment_intent.canceled';
+export const STRIPE_EVENT_PAYMENT_FAILED = 'payment_intent.payment_failed';
+
 class StripeService {
   private stripeInstance: Stripe;
   constructor() {
     this.stripeInstance = new Stripe(APP_STRIPE_API_KEY);
   }
+
+  verifyWebhookSignature = ({
+    signature,
+    body,
+    webhookSecret,
+  }: {
+    signature: string;
+    body: Buffer;
+    webhookSecret: string;
+  }) => {
+    return this.stripeInstance.webhooks.constructEvent(body, signature, webhookSecret);
+  };
 
   createPaymentIntent = async ({
     price,
